733085fd101388bddded466301eb4271d12a2a8b
Ajfaust <github.sqp1a@mail.kagimi.com>
2024-05-12 21:11:25 -0700

    Start incorporating react aria routing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,25 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRoute, useRouter, type NavigateOptions, type RegisteredRouter, type ToOptions } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { RouterProvider } from 'react-aria-components';
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RootRoute
+});
+
+declare module 'react-aria-components' {
+  interface RouterConfig {
+    href: ToPathOption<RegisteredRouter>;
+    routerOptions: Omit<NavigateOptions, keyof ToOptions>;
+  }
+}
+
+function RootRoute() {
+  let router = useRouter();
+  return (
     <>
+      <RouterProvider navigate={(to: string, options: ToOptions) => router.navigate({to, ...options})} useHref={(to: string) => router.buildLocation(to).href} />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   )
-});
+}
